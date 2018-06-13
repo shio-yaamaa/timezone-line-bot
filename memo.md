b3f214baf754bb3131f062cc2a0929ab00ac1b9f
Shiori <shio.yama520@gmail.com>
2018-06-13 16:34:28 +0000

    connected to DynamoDB

--- memo.md
+++ memo.md
@@ -7,8 +7,14 @@
 - Timezone list might not be up-to-date because it is statically defined in the script.
     It might be better to create a DB table for it, but I wanna keep the cost for DynamoDB as low as possible.
 
+# Missing credentials
+
+- Set the profile to be default
+- Put double quotations
+
 # Todo
 
+- Revise the reply message
 - Delete redundant information in end string
 - Whitelist the IP address
 - When the location information is sent, return the official timezone name of that location
@@ -20,32 +26,8 @@
 - https://dev.classmethod.jp/etc/lambda-line-bot-tutorial/
     Refer to this page when transferring this to Amazon Lambda
 
-DB
+# DB Structure
 
-chats: {
-    users: [
-        {
-            userId: 'AAA',
-            timezones: [
-                {
-                    timezone: 'Asia/Tokyo',
-                    alias: 'jst'
-                },
-                ...
-            ]
-        },
-        ...
-    ],
-    groups: [
-        {
-            groupId: 'AAA',
-            timezones: []
-        }
-    ],
-    rooms: [
-        {
-            roomId: 'AAA'
-            timezones: []
-        }
-    ]
-}
+- Hash key: Id
+- Sort key: ChatType (either 'user', 'group', or 'room')
+- Attribute: Timezones (an JSON object with two keys: timezone and alias)
--- server.js
+++ server.js
@@ -2,12 +2,21 @@ require('dotenv').config();
 
 const http = require('http');
 const crypto = require('crypto');
+const AWS = require('aws-sdk');
 const line = require('@line/bot-sdk');
 const moment = require('moment-timezone');
 const chrono = require('chrono-node');
 
 const TIME_STRING_MAX_LENGTH = '0000/00/00 00:00:00am - 0000/00/00 00:00:00am'.length;
 const TIME_FORMAT = 'YYYY/M/D H:mm';
+const TABLE_NAME = 'TimezoneLineBot';
+AWS.config.update({
+  region: "ap-northeast-1",
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  endpoint: "dynamodb.ap-northeast-1.amazonaws.com"
+});
+const docClient = new AWS.DynamoDB.DocumentClient();
 moment.tz.setDefault('UTC');
 
 http.createServer((request, response) => {
@@ -22,29 +31,25 @@ http.createServer((request, response) => {
     if (body.events[0].type != 'message' || body.events[0].message.type != 'text') {
       return;
     }
-    const replyText = createReplyText(body.events[0].message.text);
-    if (replyText) {
-      // Construct reply message
-      const replyMessage = {
-        type: 'text',
-        text: replyText
-      };
-      
-      // Send back the reply message
-      const client = new line.Client({channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN});
-      const replyToken = body.events[0].replyToken;
-      client.replyMessage(replyToken, replyMessage)
-        .then(() => {
-          console.log(replyMessage);
-        })
-        .catch(error => {
-          console.log(error);
-        });
+    const chatroomInfo = {type: body.events[0].source.type};
+    switch (body.events[0].source.type) {
+      case 'user':
+        chatroomInfo.id = body.events[0].source.userId;
+        break;
+      case 'group':
+        chatroomInfo.id = body.events[0].source.groupId;
+        break;
+      case 'room':
+        chatroomInfo.id = body.events[0].source.roomId;
+        break;
+      default:
+        break;
     }
+    reply(body.events[0].replyToken, chatroomInfo, body.events[0].message.text);
     
     response.writeHead(200, {
       'Content-Type': 'application/json',
-      'Authorization': `Bearer {${process.env.CHANNEL_ACCESS_TOKEN}}`
+      'Authorization': `Bearer {${process.env.LINE_CHANNEL_ACCESS_TOKEN}}`
     });
     response.end();
   });
@@ -66,41 +71,63 @@ const parseRequestBody = (request, callback) => {
 };
 
 const validateSignature = (signature, body) => {
-  return signature == crypto.createHmac('sha256', process.env.CHANNEL_SECRET)
+  return signature == crypto.createHmac('sha256', process.env.LINE_CHANNEL_SECRET)
     .update(JSON.stringify(body))
     .digest('base64');
 };
 
+const sendReply = (replyToken, replyText) => {
+  if (replyText) {
+    // Construct reply message
+    const replyMessage = {
+      type: 'text',
+      text: replyText
+    };
+    
+    // Send back the reply message
+    const client = new line.Client({channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN});
+    client.replyMessage(replyToken, replyMessage)
+      .then(() => {
+        console.log(replyMessage);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+};
+
 // Returns null if the bot shouldn't reply
-const createReplyText = receivedText => {
+const reply = (replyToken, chatroomInfo, receivedText) => {
   // Timezone commands
   const words = receivedText.replace(/\s\s+/g, ' ').split(' ');
   if (words.length >= 2 && words[0] === 'timezone') {
     switch (words[1]) {
       case 'help':
-        return getCommandList();
+        sendCommandList(replyToken);
+        break;
       case 'all':
-        return getAllTimezones();
+        sendAllTimezones(replyToken);
+        break;
       case 'list':
-        return getTimezoneList();
+        sendTimezoneList(replyToken, chatroomInfo);
+        break;
       case 'now':
-        return getCurrentTime();
+        sendCurrentTime(replyToken, chatroomInfo);
+        break;
       case 'add':
         if (words.length >= 3) { // Timezone (words[2]) is specified
           if (words.length >= 5 && words[3] === 'as') {
-            return addTimezone(words[2], words[4]);
+            addTimezone(replyToken, chatroomInfo, words[2], words[4]);
           } else {
-            return addTimezone(words[2], null);
+            addTimezone(replyToken, chatroomInfo, words[2], null);
           }
-        } else {
-          break;
         }
+        break;
       case 'remove':
         if (words.length >= 3) { // Timezone (words[2]) is specified
-          return removeTimezone(words[2]);
-        } else {
-          break;
+          removeTimezone(replyToken, chatroomInfo, words[2]);
         }
+        break;
       default:
         break;
     }
@@ -110,7 +137,6 @@ const createReplyText = receivedText => {
   const parsedReceivedText = parseTime(receivedText);
   if (parsedReceivedText.some(element => element.type === 'time')) { // If parsedReceivedText contains time
     // Construct reply message
-    console.log(parsedReceivedText);
     return parsedReceivedText.map((element, index) => {
       switch (element.type) {
         case 'text':
@@ -137,7 +163,19 @@ const escapeRegExp = string => {
   return string.replace(/[\\^$*+?.()|[\]{}]/g, '\\$&');
 };
 
-const getCommandList = () => {
+// The callback function will receive (error, data). The chatroom item is data.Item
+const getChatroomFromDB = (chatroomInfo, callback) => {
+  const chatroomGetParams = {
+    TableName: TABLE_NAME,
+    Key: {
+      Id: chatroomInfo.id,
+      ChatType: chatroomInfo.type
+    }
+  };
+  docClient.get(chatroomGetParams, callback);
+};
+
+const sendCommandList = () => {
   return 'timezone all: Send the link to the list of all the timezones\n\n'
     + 'timezone list: Show the list of registered timezones in this chatroom\n\n'
     + 'timezone now: Show the current time in all the timezones registered in this chatroom\n\n'
@@ -146,37 +184,142 @@ const getCommandList = () => {
     + 'Put the alias of the timezone before the time to convert it to other timezones';
 };
 
-const getAllTimezones = () => {
-  return 'link to the list of all timezones';
+const sendAllTimezones = (replyToken) => {
+  sendReply(replyToken, 'link to the list of all timezones');
 };
 
-const getTimezoneList = () => {
-  return 'list of the registered timezones';
+const sendTimezoneList = (replyToken, chatroomInfo) => {
+  getChatroomFromDB(chatroomInfo, (error, data) => {
+    if (error) {
+      console.log(error);
+      sendReply('Error retrieving your chatroom\'s data');
+    } else {
+      if (data.Item && data.Item.Timezones.length > 0) { // The chatroom exists and there is at least one timezone
+        sendReply(replyToken, data.Item.Timezones.map((timezoneObject, index) => {
+          return `timezone: ${timezoneObject.timezone}\nalias: ${timezoneObject.alias}`;
+        }).join('\n\n'));
+      } else {
+        sendReply(replyToken, 'No timezone in this chatroom');
+      }
+    }
+  });
 };
 
-const getCurrentTime = () => {
-  return 'current time';
+const sendCurrentTime = (replyToken, chatroomInfo) => {
+  sendReply(replyToken, 'current time');
 };
 
-const addTimezone = (timezone, alias) => {
-  if (moment.tz.names().includes(timezone)) { // If timezone exists
-    //    if the timezone already exists in the timezone list in this chatroom:
-    //      just update the alias
-    //    else:
-    //      add timezone to the chatroom's timezone list with the specified alias (in the case alias is null, alias should be the same as timezone)
-    //    return 'Added timezone as alias'
-    return `add ${timezone}` + (alias ? ` as ${alias}` : '');
+const addTimezone = (replyToken, chatroomInfo, timezone, alias) => {
+  if (moment.tz.names().includes(timezone)) { // If the timezone is valid
+    getChatroomFromDB(chatroomInfo, (error, data) => {
+      if (error) {
+        console.log(error);
+        sendReply(replyToken, 'Error retrieving your chatroom\'s data');
+      } else {
+        if (data.Item) { // The chatroom already exists in the DB
+          const newTimezones = data.Item.Timezones;
+          const timezoneIndexInChatroom = data.Item.Timezones.reduce((accumulator, currentTimezone, index) => {
+            if (currentTimezone.timezone === timezone) {
+              return index;
+            } else {
+              return accumulator;
+            }
+          }, null);
+          if (timezoneIndexInChatroom != null) { // The timezone has already been registered in the chatroom
+            newTimezones[timezoneIndexInChatroom] = {timezone: timezone, alias: alias ? alias : timezone};
+          } else {
+            newTimezones.push({timezone: timezone, alias: alias ? alias : timezone});
+          }
+          // Update the timezones
+          const updateParams = {
+            TableName: TABLE_NAME,
+            Key: {
+              Id: chatroomInfo.id,
+              ChatType: chatroomInfo.type
+            },
+            UpdateExpression: 'set Timezones = :t',
+            ExpressionAttributeValues:{':t': newTimezones},
+            ReturnValues: 'UPDATED_NEW'
+          };
+          docClient.update(updateParams, (error, data) => {
+            if (error) {
+              console.log(error);
+              sendReply(replyToken, 'Error updating the timezone');
+            } else {
+              sendReply(replyToken, 'Update done!');
+            }
+          });
+        } else { // The chatroom doesn't exist
+          const chatroomPutParams = {
+            TableName: TABLE_NAME,
+            Item: {
+              Id: chatroomInfo.id,
+              ChatType: chatroomInfo.type,
+              Timezones: [{timezone: timezone, alias: alias ? alias : timezone}]
+            }
+          };
+          docClient.put(chatroomPutParams, (error, data) => {
+            if (error) {
+              console.log(error);
+              sendReply(replyToken, 'Error adding your chatroom to the DB');
+            } else {
+              sendReply(replyToken, 'Your chatroom has been added to the DB!');
+            }
+          });
+        }
+      }
+    });
   } else {
-    return 'No such timezone';
+    sendReply(replyToken, 'No such timezone');
   }
 };
 
 // Timezone is either official name or alias
-const removeTimezone = timezone => {
-  
+const removeTimezone = (replyToken, chatroomInfo, timezone) => {
+  getChatroomFromDB(chatroomInfo, (error, data) => {
+    if (error) {
+      console.log(error);
+      sendReply('Error retrieving your chatroom\'s data');
+    } else {
+      if (data.Item) { // The chatroom exists in the DB
+        const timezoneIndexInChatroom = data.Item.Timezones.reduce((accumulator, currentTimezone, index) => {
+          if (currentTimezone.timezone === timezone || currentTimezone.alias === timezone) {
+            return index;
+          } else {
+            return accumulator;
+          }
+        }, null);
+        if (timezoneIndexInChatroom != null) { // The timezone has been registered in this chatroom
+          const newTimezones = data.Item.Timezones;
+          newTimezones.splice(timezoneIndexInChatroom, 1);
+          const updateParams = {
+            TableName: TABLE_NAME,
+            Key: {
+              Id: chatroomInfo.id,
+              ChatType: chatroomInfo.type
+            },
+            UpdateExpression: 'set Timezones = :t',
+            ExpressionAttributeValues:{':t': newTimezones},
+            ReturnValues: 'UPDATED_NEW'
+          };
+          docClient.update(updateParams, (error, data) => {
+            if (error) {
+              console.log(error);
+              sendReply(replyToken, 'Error removing the timezone');
+            } else {
+              sendReply(replyToken, 'The timezone has been removed!');
+            }
+          });
+          return;
+        }
+      }
+      // Either the chatroom or the timezone doesn't exist in the DB
+      sendReply(replyToken, 'That timezone hasn\'t been registered in this chatroom');
+    }
+  });
 };
 
-const aliasToTimezone = alias => {
+const aliasToTimezone = alias => { // In the future, it will receive the DB item so that it doesn't have to do an asynchronous process
   if (alias === 'jst') {
     return 'Asia/Tokyo';
   } else {
